6b988fddc274311c6acf51d2af54d43b96c6e77d
rahmah <rahmahtriagustina15@gmail.com>
2022-09-27 10:42:31 +0700

    Initial commit

---
+++ src/main/java/com/indocyber/usermgmt/UsermgmtApplication.java
@@ -0,0 +1,13 @@
+package com.indocyber.usermgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermgmtApplication.class, args);
+	}
+
+}
